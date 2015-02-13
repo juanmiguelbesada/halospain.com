eadd6260cb1f376174135a793893d4ca4b09cc89
Juan Miguel Besada <soutlink@gmail.com>
2015-02-13 13:57:34 +0100

    Update wordpress plugin

--- wp-content/plugins/custom-sidebars/inc/class-custom-sidebars-widgets.php
+++ wp-content/plugins/custom-sidebars/inc/class-custom-sidebars-widgets.php
@@ -1,51 +1,51 @@
-<?php
-
-add_action( 'cs_init', array( 'CustomSidebarsWidgets', 'instance' ) );
-
-/**
- * Extends the widgets section to add the custom sidebars UI elements.
- */
-class CustomSidebarsWidgets extends CustomSidebars {
-
-	/**
-	 * Returns the singleton object.
-	 *
-	 * @since  2.0
-	 */
-	public static function instance() {
-		static $Inst = null;
-
-		if ( null === $Inst ) {
-			$Inst = new CustomSidebarsWidgets();
-		}
-
-		return $Inst;
-	}
-
-	/**
-	 * Constructor is private -> singleton.
-	 *
-	 * @since  2.0
-	 */
-	private function __construct() {
-		if ( is_admin() ) {
-			add_action(
-				'widgets_admin_page',
-				array( $this, 'widget_sidebar_content' )
-			);
+<?php
+
+add_action( 'cs_init', array( 'CustomSidebarsWidgets', 'instance' ) );
+
+/**
+ * Extends the widgets section to add the custom sidebars UI elements.
+ */
+class CustomSidebarsWidgets extends CustomSidebars {
+
+	/**
+	 * Returns the singleton object.
+	 *
+	 * @since  2.0
+	 */
+	public static function instance() {
+		static $Inst = null;
+
+		if ( null === $Inst ) {
+			$Inst = new CustomSidebarsWidgets();
+		}
+
+		return $Inst;
+	}
+
+	/**
+	 * Constructor is private -> singleton.
+	 *
+	 * @since  2.0
+	 */
+	private function __construct() {
+		if ( is_admin() ) {
+			add_action(
+				'widgets_admin_page',
+				array( $this, 'widget_sidebar_content' )
+			);
 
 			add_action(
 				'admin_head-widgets.php',
 				array( $this, 'init_admin_head' )
 			);
-		}
-	}
-
-	/**
-	 * Adds the additional HTML code to the widgets section.
-	 */
-	public function widget_sidebar_content() {
-		include CSB_VIEWS_DIR . 'widgets.php';
+		}
+	}
+
+	/**
+	 * Adds the additional HTML code to the widgets section.
+	 */
+	public function widget_sidebar_content() {
+		include CSB_VIEWS_DIR . 'widgets.php';
 	}
 
 	/**
@@ -68,6 +68,6 @@ class CustomSidebarsWidgets extends CustomSidebars {
 	public function admin_body_class( $classes ) {
 		$classes .= ' no-auto-init ';
 		return $classes;
-	}
-
+	}
+
 };
